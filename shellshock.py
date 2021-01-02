4fb43d81308eb26fa2a5e7f4fc580191feca6b3f
Henry Haustein <henrydatei@web.de>
2021-01-02 19:17:45 +0100

    found a working solution to calc power and angle

--- shellshock.py
+++ shellshock.py
@@ -1,18 +1,17 @@
 import math
 from pynput.mouse import Listener as MouseListener
 from pynput import keyboard
+from pynput.keyboard import Key, Controller
+import time
 
-#velocity = 65 # m/s
-#angle = 50 # deg
-g = 5.5 # fern
-#g = 3.3 # nah
-#g = 3.8 # von Clemens
-#g = 4.3 # mittel
-#startHeight = 91.6 # px
+keys = Controller()
 
 def calcVelocity(distancex,distancey,angle):
-    v0 = math.sqrt((pow(distancex,2) * g)/(distancex * math.sin(2*math.radians(angle)) - 2 * distancey * pow(math.cos(math.radians(angle)),2)))
-    return v0 # px/s
+    # from https://steamcommunity.com/sharedfiles/filedetails/?id=1327582953
+    g = -379.106
+    q = 0.0518718
+    v0 = -2/(g * q) * math.sqrt((-g * distancex * distancex)/(2 * math.cos(math.radians(angle)) * math.cos(math.radians(angle)) * (math.tan(math.radians(angle)) * distancex - distancey)))
+    return v0
 
 def calcOptimal(diffx,diffy):
     smallestVelocity = 100
@@ -31,7 +30,7 @@ def calcOptimal(diffx,diffy):
 
 def calcHighestBelow100(diffx,diffy):
     for possibleAngle in range(1,90):
-        v0 = calcVelocity(diffx,diffy,90-possibleAngle)
+        v0 = calcVelocity2(diffx,diffy,90-possibleAngle)
         if v0 < 100:
             break
 
@@ -43,6 +42,34 @@ def cleanGlobals():
     del globals()['YourY']
     del globals()['EnemyX']
     del globals()['EnemyY']
+
+def setPowerAndAngle(targetPower,targetAngle,startPower,startAngle,direction):
+    diffPower = targetPower - startPower
+    if direction == "left":
+        diffAngle = targetAngle - startAngle
+    else:
+        diffAngle = -targetAngle + startAngle
+    time.sleep(1)
+    if diffPower > 0:
+        for i in range(0,diffPower):
+            # press arrow up
+            keys.tap(Key.up)
+            time.sleep(0.1)
+    else:
+        for i in range(0,-diffPower):
+            # press arrow down
+            keys.tap(Key.down)
+            time.sleep(0.1)
+    if diffAngle > 0:
+        for i in range(0,diffAngle):
+            # press arrow right
+            keys.tap(Key.right)
+            time.sleep(0.1)
+    else:
+        for i in range(0,-diffAngle):
+            # press arrow left
+            keys.tap(Key.left)
+            time.sleep(0.1)
 
 def posPlayer(x, y, button, pressed):
     if pressed:
@@ -85,7 +112,7 @@ def EnemyLocation():
     if 'YourX' in globals() and 'YourY' in globals():
         # all needed, calc shot
         diffx = abs(YourX-EnemyX)
-        diffy = EnemyY-YourY
+        diffy = -EnemyY+YourY
         print(str(diffx))
         print(str(diffy))
         calcOptimal(diffx,diffy)
